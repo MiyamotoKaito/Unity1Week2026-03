9e5c5745495e1364b16b527481b2335c4384270b
kenkatou354 <kenkatou354@gmail.com>
2026-03-16 09:14:21 +0900

    [add]カードクラスを追加

---
+++ Unity1Week2026.03/Assets/People/Abe/Card/Card.cs
@@ -0,0 +1,41 @@
+using Unity.VectorGraphics;
+using UnityEngine;
+/// <summary>
+/// カードを管理するクラス
+/// </summary>
+public class Card 
+{
+   public Card(int cardId, CardEffect cardEffect, CardFrontSprite cardFrontSprite, CardBackText cardBackText)
+   {
+       _cardId = new CardId(cardId);
+       _cardEffect = cardEffect;
+       _cardFrontSprite = cardFrontSprite;
+       _cardBackText = cardBackText;
+   }
+
+   public int GetCardId()
+   {
+       return _cardId.GetId();
+   }
+
+   public Sprite GetCardFrontSprite()
+   {
+       return _cardFrontSprite.GetSprite();
+   }
+
+    public string GetCardBackText()
+    {
+         return _cardBackText.GetText();
+    }
+
+   public void ExcuteEffect()
+   {
+       _cardEffect.Exucute();
+   }
+
+   private CardId _cardId;
+   private CardEffect _cardEffect;
+   private CardFrontSprite _cardFrontSprite;
+
+   private CardBackText _cardBackText;
+}
