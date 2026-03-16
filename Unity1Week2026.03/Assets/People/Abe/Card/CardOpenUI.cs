8abc8636feab451935b12985dba1a314cf9a90d6
kenkatou354 <kenkatou354@gmail.com>
2026-03-16 14:23:34 +0900

    [add]入力を受け取る処理を追加

--- Unity1Week2026.03/Assets/People/Abe/Card/CardOpenUI.cs
+++ Unity1Week2026.03/Assets/People/Abe/Card/CardOpenUI.cs
@@ -1,10 +1,18 @@
+using System;
+using TMPro;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class CardOpenUI : MonoBehaviour
 {
     [SerializeField] private CardController _cardController;
-    
-    public void OpenCard(string cardText)
+    [SerializeField] private TMP_InputField _inputField;
+
+    private void Start()
+    {
+        _inputField.onSubmit.AddListener(OpenCard);
+    }
+    private void OpenCard(string cardText)
     {
         var card = _cardController.CardRepository.FindCardByText(cardText);
         if (card != null)
