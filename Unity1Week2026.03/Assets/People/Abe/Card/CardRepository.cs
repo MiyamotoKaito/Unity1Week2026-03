0e4890ed9fee4d3a7c84180882f8e4fe5fd40fe1
kenkatou354 <kenkatou354@gmail.com>
2026-03-16 13:26:18 +0900

    [add] カードのマッチ処理を追加

--- Unity1Week2026.03/Assets/People/Abe/Card/CardRepository.cs
+++ Unity1Week2026.03/Assets/People/Abe/Card/CardRepository.cs
@@ -14,7 +14,7 @@ public class CardRepository
     {
         _cards.Add(card);
     }
-    
+
     public Card FindCardByText(string text)
     {
         foreach (var card in _cards)
@@ -41,10 +41,82 @@ public class CardRepository
         return false;
     }
 
+    public void OpenCard(Card card)
+    {
+        foreach (var c in _cards)
+        {
+            if (c.GetCardBackText().Equals(card.GetCardBackText()))
+            {
+                c.OpenCard();
+                TryResolveOpenPair();
+                return;
+            }
+        }
+    }
+    public bool TryResolveOpenPair()
+    {
+        var first = GetFirstOpenCard();
+        if (first == null)
+        {
+            return false;
+        }
+
+        var second = GetSecondOpenCard(first);
+        if (second == null)
+        {
+            return false;
+        }
+
+        var match = first.Equals(second);
+        if (match)
+        {
+            first.ExcuteEffect(); //どらちからの効果を発動させればよい。
+           // second.ExcuteEffect();
+            RemoveMatchCard(first, second);
+            return true;
+        }
+
+        first.CloseCard();
+        second.CloseCard();
+        return false;
+    }
+
+    public Card GetFirstOpenCard()
+    {
+        foreach (var card in _cards)
+        {
+            if (card.IsOpen())
+            {
+                return card;
+            }
+        }
+        return null;
+    }
+
     public void ClearCards()
     {
         _cards.Clear();
     }
 
     private List<Card> _cards = new List<Card>();
+
+    private Card GetSecondOpenCard(Card first)
+    {
+        foreach (var card in _cards)
+        {
+            if (card != first && card.IsOpen())
+            {
+                return card;
+            }
+        }
+
+        return null;
+    }
+
+
+    private void RemoveMatchCard(Card first, Card second)
+    {
+        _cards.RemoveAll(card => card.MatchCardText(first.GetCardBackText()) ||
+         card.MatchCardText(second.GetCardBackText()));
+    }
 }
