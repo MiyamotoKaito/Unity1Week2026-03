5e75425cd81368e4a11e7b4113848db879154991
kenkatou354 <kenkatou354@gmail.com>
2026-03-16 12:01:39 +0900

    [add] 生成システムをペアで生成するように変更

--- Unity1Week2026.03/Assets/People/Abe/Card/CardService/CardSpawnService.cs
+++ Unity1Week2026.03/Assets/People/Abe/Card/CardService/CardSpawnService.cs
@@ -1,8 +1,8 @@
 using UnityEngine;
 /// <summary>
-/// 同じカードが複数枚存在しないようにするためのクラス
+/// 蜷後§繧ｫ繝ｼ繝峨′隍・焚譫壼ｭ伜惠縺励↑縺・ｈ縺・↓縺吶ｋ縺溘ａ縺ｮ繧ｯ繝ｩ繧ｹ
 /// </summary>
-public class CardSpawnSystem 
+public class CardSpawnSystem
 {
     private CardRepository _cardRepository;
 
@@ -10,20 +10,42 @@ public class CardSpawnSystem
     {
         _cardRepository = cardRepository;
     }
- 
-    public void SpawnCard(Card card)
+
+    public bool SpawnCardPair(CardData cardData, string textA, string textB)
     {
-        if (!_cardRepository.CardExists(card.GetCardId(), card.GetCardBackText()))
+        if (string.IsNullOrWhiteSpace(textA) || string.IsNullOrWhiteSpace(textB))
         {
-            _cardRepository.AddCard(card);
-            //TODO カードの裏面のテキストをタイピングシステムから受け取る。
-            var cardObject = new GameObject("Card");
-            var cardView = cardObject.AddComponent<CardView>();
-            cardView.SetCard(card);
-            //TODO カードの位置を決める。
-            //TODO カードを２枚生成する。
+            return false;
         }
 
-        
+        if (textA == textB)
+        {
+            return false;
+        }
+
+        if (_cardRepository.TextExists(textA) || _cardRepository.TextExists(textB))
+        {
+            return false;
+        }
+
+        var cardA = new Card(cardData.CardId, cardData.CardEffect, cardData.FrontSprite, textA);
+        var cardB = new Card(cardData.CardId, cardData.CardEffect, cardData.FrontSprite, textB);
+
+        _cardRepository.AddCard(cardA);
+        _cardRepository.AddCard(cardB);
+
+        CreateCardObject(cardA);
+        CreateCardObject(cardB);
+        return true;
+    }
+
+    private void CreateCardObject(Card card)
+    {
+        //TODO 繧ｫ繝ｼ繝峨・陬城擇縺ｮ繝・く繧ｹ繝医ｒ繧ｿ繧､繝斐Φ繧ｰ繧ｷ繧ｹ繝・Β縺九ｉ蜿励￠蜿悶ｋ縲・
+        var cardObject = new GameObject("Card");
+        var cardView = cardObject.AddComponent<CardView>();
+        cardView.SetCard(card);
+        //TODO 繧ｫ繝ｼ繝峨・菴咲ｽｮ繧呈ｱｺ繧√ｋ縲・
+        //TODO 繧ｫ繝ｼ繝峨ｒ・呈椢逕滓・縺吶ｋ縲・
     }
 }
