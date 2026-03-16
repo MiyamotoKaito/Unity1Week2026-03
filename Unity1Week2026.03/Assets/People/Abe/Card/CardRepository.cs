0eceef438eb1825079c629e2f4a3a470d49fb469
kenkatou354 <kenkatou354@gmail.com>
2026-03-16 09:17:10 +0900

    [add] 盤面のカード管理クラス

---
+++ Unity1Week2026.03/Assets/People/Abe/Card/CardRepository.cs
@@ -0,0 +1,32 @@
+using System.Collections.Generic;
+
+/// <summary>
+/// 盤面のカードを管理するクラス
+/// </summary>
+public class CardRepository
+{
+    public CardRepository()
+    {
+
+    }
+
+    public void AddCard(Card card)
+    {
+        _cards.Add(card);
+    }
+
+    public bool CardExists(int id,string text)
+    {
+        foreach (var card in _cards)
+        {
+            if (id == card.GetCardId() && text == card.GetCardBackText())
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    private List<Card> _cards = new List<Card>();
+}
