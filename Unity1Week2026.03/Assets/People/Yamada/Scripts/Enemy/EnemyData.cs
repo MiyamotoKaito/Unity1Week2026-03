13093705e3f85ee3498657a880f912285187313a
ayato0913a <ayato0913a@gmail.com>
2026-03-18 07:46:55 +0900

    [ feat ] 敵のデータを管理するデータクラスを追加

--- Unity1Week2026.03/Assets/People/Yamada/Scripts/Enemy/EnemyData.cs
+++ Unity1Week2026.03/Assets/People/Yamada/Scripts/Enemy/EnemyData.cs
@@ -1,7 +1,27 @@
 using UnityEngine;
 
+/// <summary>
+///     敵のデータを管理するScriptableObjectクラス。
+/// </summary>
 [CreateAssetMenu(fileName = "EnemyData", menuName = "Scriptable Objects/EnemyData")]
 public class EnemyData : ScriptableObject
 {
+    public string EnemyName => _enemyName;
+    public Sprite Sprite => _sprite;
+    public int MaxHealth => _maxHealth;
+    public int AttackPower => _attackPower;
+    public float AttackIntervalSeconds => _attackIntervalSeconds;
+    public int SkillTurnInterval => _skillTurnInterval;
 
+    [Header("基本情報")]
+    [SerializeField] private string _enemyName;
+    [SerializeField] private Sprite _sprite;
+
+    [Header("ステータス")]
+    [SerializeField] private int _maxHealth;
+    [SerializeField] private int _attackPower;
+
+    [Header("行動設定")]
+    [SerializeField] private float _attackIntervalSeconds;
+    [SerializeField] private int _skillTurnInterval;
 }
