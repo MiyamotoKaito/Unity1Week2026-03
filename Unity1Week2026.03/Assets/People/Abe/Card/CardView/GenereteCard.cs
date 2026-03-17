cbab87a6797766de7bd61b5c8444e0623542e365
kenkatou354 <kenkatou354@gmail.com>
2026-03-17 16:05:55 +0900

    [add]カードの生成処理を変更

--- Unity1Week2026.03/Assets/People/Abe/Card/CardView/GenereteCard.cs
+++ Unity1Week2026.03/Assets/People/Abe/Card/CardView/GenereteCard.cs
@@ -1,21 +1,21 @@
-using Unity.VisualScripting;
+
+using System.Collections.Generic;
 using UnityEngine;
 
-public class GenerateCardView : MonoBehaviour
+public class CardViewSpawn : MonoBehaviour
 {
+    public List<GameObject> GenerateCard(int count)
+    {
+        var cards = new List<GameObject>();
+        for (int i = 0; i < count; i++)
+        {
+            var card = Instantiate(_cardPrefab, _cardParent);
+            cards.Add(card);
+        }
+        return cards;
+    }
     [SerializeField] private GameObject _cardPrefab;
     [SerializeField] private Transform _cardParent;
-    [SerializeField] private CardController _cardController;
 
-    private void OnEnable()
-    {
-        GenerateCard();
-    }
-    private void GenerateCard()
-    {
-        for (int i = 0; i < _cardController.MaxCardPairs * 2; i++)
-        {
-           Instantiate(_cardPrefab, _cardParent);
-        }
-    }
+
 }
