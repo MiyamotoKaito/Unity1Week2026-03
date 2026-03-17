58f900c54a74c1f07de633a8f1bb444d3bb44918
ayato0913a <ayato0913a@gmail.com>
2026-03-18 07:18:06 +0900

    [ feat ] プレイヤーのHPを管理するプレゼンター管理するクラスを作成

--- Unity1Week2026.03/Assets/People/Yamada/Scripts/Player/PlayerHealthPresenter.cs
+++ Unity1Week2026.03/Assets/People/Yamada/Scripts/Player/PlayerHealthPresenter.cs
@@ -1,16 +1,40 @@
 using UnityEngine;
 
-public class PlayerHealthPresenter : MonoBehaviour
+namespace Unity1Week.URA.Player
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    /// <summary>
+    ///     プレイヤーのHPを管理するプレゼンタークラス。
+    /// </summary>
+    public class PlayerHealthPresenter : MonoBehaviour
     {
-        
-    }
+        private void HandleHealthChanged(int currentHealth, int maxHealth)
+        {
+            _healthView.UpdateHealthBar(currentHealth, maxHealth);
+        }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        private void HandleDied()
+        {
+            Debug.Log("プレイヤー死亡！");
+        }
+
+        private void Awake()
+        {
+            _healthModel = new PlayerHealthModel(_maxHealth);
+            _healthModel.OnHealthChanged += HandleHealthChanged;
+            _healthModel.OnPlayerDied += HandleDied;
+
+            _healthView.UpdateHealthBar(_healthModel.CurrentHealth, _healthModel.MaxHealth);
+        }
+
+        private void OnDestroy()
+        {
+            _healthModel.OnHealthChanged -= HandleHealthChanged;
+            _healthModel.OnPlayerDied -= HandleDied;
+        }
+
+        [SerializeField] private PlayerHealthView _healthView;
+        [SerializeField] private int _maxHealth;
+
+        private PlayerHealthModel _healthModel;
     }
 }
