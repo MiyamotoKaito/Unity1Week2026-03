464e947f4b64a59f9e9391871112eb2ccfe646ea
ayato0913a <ayato0913a@gmail.com>
2026-03-18 07:23:26 +0900

    [ fix ] コメント追加

--- Unity1Week2026.03/Assets/People/Yamada/Scripts/Player/PlayerHealthPresenter.cs
+++ Unity1Week2026.03/Assets/People/Yamada/Scripts/Player/PlayerHealthPresenter.cs
@@ -7,11 +7,19 @@ namespace Unity1Week.URA.Player
     /// </summary>
     public class PlayerHealthPresenter : MonoBehaviour
     {
+        /// <summary>
+        ///     HPが変更されたときViewの更新を呼び出す。
+        /// </summary>
+        /// <param name="currentHealth"></param>
+        /// <param name="maxHealth"></param>
         private void HandleHealthChanged(int currentHealth, int maxHealth)
         {
             _healthView.UpdateHealthBar(currentHealth, maxHealth);
         }
 
+        /// <summary>
+        ///     プレイヤーが死亡したときの処理。
+        /// </summary>
         private void HandleDied()
         {
             Debug.Log("プレイヤー死亡！");
