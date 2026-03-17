79784f7795cdf0740832c3adb765acee2b40b04d
ayato0913a <ayato0913a@gmail.com>
2026-03-18 07:16:38 +0900

    [ feat ] プレイヤーのHPを管理するモデルクラスを追加

--- Unity1Week2026.03/Assets/People/Yamada/Scripts/Player/PlayerHealthModel.cs
+++ Unity1Week2026.03/Assets/People/Yamada/Scripts/Player/PlayerHealthModel.cs
@@ -1,6 +1,43 @@
+using System;
 using UnityEngine;
 
-public class PlayerHealthModel
+namespace Unity1Week.URA.Player
 {
-    
+    /// <summary>
+    ///     プレイヤーのHPを管理するモデルクラス。
+    /// </summary>
+    public class PlayerHealthModel
+    {
+        public PlayerHealthModel(int maxHealth)
+        {
+            _maxHealth = maxHealth;
+            _currentHealth = maxHealth;
+        }
+
+        public event Action<int,int> OnHealthChanged;
+        public event Action OnPlayerDied;
+
+        public int CurrentHealth => _currentHealth;
+        public int MaxHealth => _maxHealth;
+
+        public void TakeDamage(int damage)
+        {
+            _currentHealth = Mathf.Max(_currentHealth - damage, 0);
+            OnHealthChanged?.Invoke(_currentHealth, _maxHealth);
+
+            if(_currentHealth == 0)
+            {
+                OnPlayerDied?.Invoke();
+            }
+        }
+
+        public void Heal(int healAmount)
+        {
+            _currentHealth = Mathf.Min(_currentHealth + healAmount, _maxHealth);
+            OnHealthChanged?.Invoke(_currentHealth, _maxHealth);
+        }
+
+        private int _currentHealth;
+        private int _maxHealth;
+    }
 }
