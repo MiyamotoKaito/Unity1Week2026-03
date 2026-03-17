28d288ff5a54e0d6fbbe2c5b32fd1eb4b220fae5
kenkatou354 <kenkatou354@gmail.com>
2026-03-17 17:17:24 +0900

    [add]パスを変更

--- Unity1Week2026.03/Assets/People/Abe/Card/CardView/CardView.cs
+++ Unity1Week2026.03/Assets/People/Abe/Card/CardView/CardView.cs
@@ -7,14 +7,14 @@ public class CardView : MonoBehaviour
     public void SetCard(Card card)
     {
         
-        _image.sprite = card.GetCardFrontSprite();
+        _image.sprite = card?.GetCardFrontSprite();
         _text.text = card.GetCardBackText();
     }
 
     private Image _image;
     private TextMeshProUGUI _text;
     private const string CardBackImagePath = "CardBackImage";
-    private const string CardBackTextPath = "CardBackImage/CardBackText";
+    private const string CardBackTextPath = "CardBackText";
     private void Awake()
     {
         _image = transform.Find(CardBackImagePath).GetComponent<Image>();
