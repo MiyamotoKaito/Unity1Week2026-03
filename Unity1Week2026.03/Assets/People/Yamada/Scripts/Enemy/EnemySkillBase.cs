ee87bcbb8b2cc4fdd6e0dc4addce003ad1702ad7
ayato0913a <ayato0913a@gmail.com>
2026-03-18 17:59:11 +0900

    [ feat ] 敵スキルの基底クラスを作成

--- Unity1Week2026.03/Assets/People/Yamada/Scripts/Enemy/EnemySkillBase.cs
+++ Unity1Week2026.03/Assets/People/Yamada/Scripts/Enemy/EnemySkillBase.cs
@@ -1,6 +1,18 @@
+using System;
 using UnityEngine;
 
-public class EnemySkillBase
+namespace Unity1Week.URA.Enemy
 {
-    
+    /// <summary>
+    ///     敵スキルの基底クラス。
+    /// </summary>
+    [Serializable]
+    public abstract class EnemySkillBase
+    {
+        /// <summary>
+        ///     スキルを実行する。
+        /// </summary>
+        /// <param name="enemySkillContext"></param>
+        public abstract void Execute(EnemySkillContext enemySkillContext);
+    }
 }
