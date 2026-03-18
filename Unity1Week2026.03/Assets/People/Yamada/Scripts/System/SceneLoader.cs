33047d1d9ac7a5b7011f4f5600f49c8a75f96228
ayato0913a <ayato0913a@gmail.com>
2026-03-19 03:16:08 +0900

    [ feat ] SceneLoader作成

--- Unity1Week2026.03/Assets/People/Yamada/Scripts/System/SceneLoader.cs
+++ Unity1Week2026.03/Assets/People/Yamada/Scripts/System/SceneLoader.cs
@@ -1,6 +1,19 @@
-using UnityEngine;
+using UnityEngine.SceneManagement;
 
-public class SceneLoader
+namespace Unity1Week.URA.System
 {
-    
+    /// <summary>
+    ///     SceneLoadをするクラス
+    /// </summary>
+    public static class SceneLoader
+    {
+        /// <summary>
+        ///     シーンをロードする
+        /// </summary>
+        /// <param name="sceneName">シーンの名前</param>
+        public static void LoadScene(string sceneName)
+        {
+            SceneManager.LoadScene(sceneName);
+        }
+    }
 }
