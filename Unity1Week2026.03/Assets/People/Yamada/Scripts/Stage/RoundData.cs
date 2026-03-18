84caa5f7f0963375acc1f418560aca459cc89e1c
ayato0913a <ayato0913a@gmail.com>
2026-03-19 04:45:18 +0900

    [ feat ] ラウンドごとのデータを管理するクラスを作成

--- Unity1Week2026.03/Assets/People/Yamada/Scripts/Stage/RoundData.cs
+++ Unity1Week2026.03/Assets/People/Yamada/Scripts/Stage/RoundData.cs
@@ -1,7 +1,17 @@
 using UnityEngine;
+using Unity1Week.URA.Enemy;
 
-[CreateAssetMenu(fileName = "RoundData", menuName = "Scriptable Objects/RoundData")]
-public class RoundData : ScriptableObject
+namespace Unity1Week.URA.Stage
 {
-    
+    /// <summary>
+    ///     ラウンドごとのデータを管理するクラス。
+    /// </summary>
+    [CreateAssetMenu(fileName = "RoundData", menuName = "Scriptable Objects/RoundData")]
+    public class RoundData : ScriptableObject
+    {
+        public EnemyData EnemyData => _enemyData;
+
+        [SerializeField] private EnemyData _enemyData;
+        //TODO: 背景とかラウンドごとに必要なデータがあればここに追加
+    }
 }
