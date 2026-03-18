e11554dcf3059df3f593427148033f048a984415
ayato0913a <ayato0913a@gmail.com>
2026-03-19 04:44:29 +0900

    [ feat ] ステージのデータを管理するクラスを作成

--- Unity1Week2026.03/Assets/People/Yamada/Scripts/Stage/StageData.cs
+++ Unity1Week2026.03/Assets/People/Yamada/Scripts/Stage/StageData.cs
@@ -1,7 +1,19 @@
+using System.Collections.Generic;
 using UnityEngine;
 
-[CreateAssetMenu(fileName = "StageData", menuName = "Scriptable Objects/StageData")]
-public class StageData : ScriptableObject
+namespace Unity1Week.URA.Stage
 {
-    
+    /// <summary>
+    ///     ステージのデータを管理するクラス。
+    ///     ステージ名と、ステージ内のラウンドデータのリストを持つ。
+    /// </summary>
+    [CreateAssetMenu(fileName = "StageData", menuName = "Scriptable Objects/StageData")]
+    public class StageData : ScriptableObject
+    {
+        public string Stagename => _stagename;
+        public IReadOnlyList<RoundData> RoundDatas => _roundDatas;
+
+        [SerializeField] private string _stagename;
+        [SerializeField] private List<RoundData> _roundDatas;
+    }
 }
