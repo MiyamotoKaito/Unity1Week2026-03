abaeb160a1f958676158210cb2426d1635765b0d
ayato0913a <ayato0913a@gmail.com>
2026-03-18 17:59:49 +0900

    [ feat ] 敵スキル実行に必要な情報をまとめたクラスを作成

--- Unity1Week2026.03/Assets/People/Yamada/Scripts/Enemy/EnemySkillContext.cs
+++ Unity1Week2026.03/Assets/People/Yamada/Scripts/Enemy/EnemySkillContext.cs
@@ -1,6 +1,24 @@
+using Unity1Week.URA.Player;
 using UnityEngine;
 
-public class EnemySkillContext
+namespace Unity1Week.URA.Enemy
 {
-    
+    /// <summary>
+    ///     敵スキルの実行に必要な情報をまとめたクラス。
+    /// </summary>
+    public class EnemySkillContext
+    {
+        public EnemySkillContext(EnemyRuntimeModel enemyRuntimeModel,
+            PlayerHealthModel playerHealthModel,
+            EnemySkillTurnTracker enemySkillTurnTracker)
+        {
+            EnemyRuntimeModel = enemyRuntimeModel;
+            PlayerHealthModel = playerHealthModel;
+            EnemySkillTurnTracker = enemySkillTurnTracker;
+        }
+
+        public EnemyRuntimeModel EnemyRuntimeModel { get; }
+        public PlayerHealthModel PlayerHealthModel { get; }
+        public EnemySkillTurnTracker EnemySkillTurnTracker { get; }
+    }
 }
