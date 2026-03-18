076db49a81d6f435dd9f2eb3e591cbc14541891c
kaitomiyamoto1 <kaitomiyamoto1@gmail.com>
2026-03-18 10:08:21 +0900

    [add]テキスト入力クラス

---
+++ Unity1Week2026.03/Assets/People/Miyamoto/Script/InputFieldController.cs
@@ -0,0 +1,39 @@
+﻿using TMPro;
+using UnityEngine;
+
+/// <summary>
+/// InputFieldを制御するクラス
+/// </summary>
+public class InputFieldController
+{
+    #region 変数
+    private TMP_InputField _inputField;
+    #endregion
+
+    #region コンストラクタ
+    public InputFieldController(TMP_InputField inputField)
+    {
+        _inputField = inputField;
+
+        ClearTextField();
+        ActioveInput();
+    }
+    #endregion
+
+    #region メソッド
+    /// <summary>
+    /// InputFieldに書かれている文字を全て削除する
+    /// </summary>
+    public void ClearTextField()
+    {
+        _inputField.text = string.Empty;
+    }
+    /// <summary>
+    /// InputFieldに文字を打てるようにする
+    /// </summary>
+    public void ActioveInput()
+    {
+        _inputField.ActivateInputField();
+    }
+    #endregion
+}
