a580bc3ed6741c4c70c57939a5a0cceab17b5175
ayato0913a <ayato0913a@gmail.com>
2026-03-20 20:42:57 +0900

    [ feat ] スキルやジャマー等の機能追加

--- Unity1Week2026.03/Assets/People/Abe/Card/EffectManager/EffectManager.cs
+++ Unity1Week2026.03/Assets/People/Abe/Card/EffectManager/EffectManager.cs
@@ -1,13 +1,18 @@
 
-using UnityEngine;
+using System.Collections.Generic;
 using Unity1Week.URA.Stage;
+using UnityEngine;
 public class EffectManager : MonoBehaviour
 {
     public static EffectManager Instance { get; private set; }
     private StageProgressController _stageProgressController;
-    public void Initialize(StageProgressController stageProgressController)
+    private CardPresenter _cardPresenter;
+    private GridCard _gridCard;
+    public void Initialize(StageProgressController stageProgressController, CardPresenter cardPresenter,GridCard gridCard)
     {
         _stageProgressController = stageProgressController;
+        _cardPresenter = cardPresenter;
+        _gridCard = gridCard;
     }
 
     public void PlayAttack(int damage)
@@ -18,11 +23,42 @@ public class EffectManager : MonoBehaviour
     {
         _stageProgressController.HealPlayer(healAmount);
     }
-
-    public void ReduceEnemySkillTurn()
+    public void ReduceEnemyAttackTimer(float amount)
     {
-        _stageProgressController.ReduceEnemySkillTurn();
+        _stageProgressController.ReduceEnemyAttackTimer(amount);
     }
+    public void ReduceEnemySkillTurn(int value = 1)
+    {
+        _stageProgressController.ReduceEnemySkillTurn(value);
+    }
+    public void ApplyMirrorFont()
+    {
+        _cardPresenter.MirrorTexts();
+    }
+    public void ApplyReverseText()
+    {
+        _cardPresenter.ReversTexts();
+    }
+    public void DuplicateJammerPair(List<CardData> dataList)
+    {
+        _cardPresenter.ReplaceCardContents(dataList);
+    }
+
+    public void ReduceCurrentPower(int value)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void ShuffleAllCardPositions()
+    {
+        _gridCard.GridCards();
+    }
+
+    public void ShuffleSomeCardTexts(int shuffleCount)
+    {
+        _cardPresenter.ShuffleSomeCards(shuffleCount);
+    }
+
     private void Awake()
     {
         if (Instance != null && Instance != this)
