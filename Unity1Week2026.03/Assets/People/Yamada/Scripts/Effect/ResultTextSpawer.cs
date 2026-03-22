651be28a3bcf54bd741245ecb99e5dcd27b74c21
ayato0913a <ayato0913a@gmail.com>
2026-03-22 20:04:18 +0900

    [ feat ] 文字を一文字ずつ生成するクラスを追加

---
+++ Unity1Week2026.03/Assets/People/Yamada/Scripts/Effect/ResultTextSpawer.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+/// <summary>
+///     文字を1文字ずつ生成するクラス。
+/// </summary>
+public class ResultTextSpawer : MonoBehaviour
+{
+    /// <summary>
+    ///     文字を生成する
+    /// </summary>
+    public List<TextMeshProUGUI> Spawn(string text)
+    {
+        Clear();
+
+        float offset = 0f;
+
+        foreach (char c in text)
+        {
+            TextMeshProUGUI charObj = Instantiate(_charPrefab, _parent);
+            charObj.text = c.ToString();
+            charObj.ForceMeshUpdate();
+
+            RectTransform rect = charObj.rectTransform;
+            rect.anchoredPosition = new Vector2(offset, 0f);
+
+            offset += charObj.preferredWidth;
+
+            _chars.Add(charObj);
+        }
+
+        return _chars;
+    }
+
+    public void Clear()
+    {
+        foreach (var c in _chars)
+        {
+            if (c != null) Destroy(c.gameObject);
+        }
+
+        _chars.Clear();
+    }
+
+    [SerializeField] private TextMeshProUGUI _charPrefab;
+    [SerializeField] private Transform _parent;
+
+    private List<TextMeshProUGUI> _chars = new();
+}
