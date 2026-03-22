c8104c5ff1f1b1eefcb36d29bf571637f8d4c790
ayato0913a <ayato0913a@gmail.com>
2026-03-22 09:50:52 +0900

    [ add ] 時間を増やす効果を追加

---
+++ Unity1Week2026.03/Assets/People/Miyamoto/Script/Effects/TimePlus.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// 
+/// </summary>
+public class TimePlus : ICardEffect
+{
+
+    public void Exucute()
+    {
+        Debug.Log("時間を止める効果が発動しました。");
+        EffectManager.Instance.AddEnemyAttackTimer(_timeToAdd);
+    }
+
+    [SerializeField] private float _timeToAdd;
+}
